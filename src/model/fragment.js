9720eb200e96a1de011337a32264c1f2ba1e559c
akakadiya1 <akakadiya1@myseneca.ca>
2025-02-07 15:30:49 -0500

    Implement a Fragment class[TDD method]

---
+++ src/model/fragment.js
@@ -0,0 +1,150 @@
+// Use crypto.randomUUID() to create unique IDs, see:
+// https://nodejs.org/api/crypto.html#cryptorandomuuidoptions
+const { randomUUID } = require('crypto');
+// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
+const contentType = require('content-type');
+
+// Functions for working with fragment metadata/data using our DB
+const {
+  readFragment,
+  writeFragment,
+  readFragmentData,
+  writeFragmentData,
+  listFragments,
+  deleteFragment,
+} = require('./data');
+
+class Fragment {
+  constructor({
+    id = randomUUID(),
+    ownerId,
+    created = new Date().toISOString(),
+    updated = new Date().toISOString(),
+    type,
+    size = 0,
+  }) {
+    if (!ownerId || !type) {
+      throw new Error('ownerId and type are required');
+    }
+    if (typeof size !== 'number' || size < 0) {
+      throw new Error('size must be a non-negative number');
+    }
+    if (!Fragment.isSupportedType(type)) {
+      throw new Error('Unsupported content type');
+    }
+    this.id = id;
+    this.ownerId = ownerId;
+    this.created = created;
+    this.updated = updated;
+    this.type = type;
+    this.size = size;
+  }
+
+  /**
+   * Get all fragments (id or full) for the given user
+   * @param {string} ownerId user's hashed email
+   * @param {boolean} expand whether to expand ids to full fragments
+   * @returns Promise<Array<Fragment>>
+   */
+  static async byUser(ownerId, expand = false) {
+    const fragments = await listFragments(ownerId, expand);
+    return expand ? fragments.map((f) => new Fragment(f)) : fragments;
+  }
+
+  /**
+   * Gets a fragment for the user by the given id.
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<Fragment>
+   */
+  static async byId(ownerId, id) {
+    // TIP: make sure you properly re-create a full Fragment instance after getting from db.
+    const fragment = await readFragment(ownerId, id);
+    if (!fragment) {
+      throw new Error('Fragment not found');
+    }
+    return new Fragment(fragment);
+  }
+
+  /**
+   * Delete the user's fragment data and metadata for the given id
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<void>
+   */
+  static async delete(ownerId, id) {
+    await deleteFragment(ownerId, id);
+  }
+
+  /**
+   * Saves the current fragment (metadata) to the database
+   * @returns Promise<void>
+   */
+  async save() {
+    this.updated = new Date().toISOString();
+    await writeFragment(this);
+  }
+
+  /**
+   * Gets the fragment's data from the database
+   * @returns Promise<Buffer>
+   */
+  getData() {
+    return readFragmentData(this.ownerId, this.id);
+  }
+
+  /**
+   * Set's the fragment's data in the database
+   * @param {Buffer} data
+   * @returns Promise<void>
+   */
+  async setData(data) {
+    // TIP: make sure you update the metadata whenever you change the data, so they match
+    if (!Buffer.isBuffer(data)) {
+      throw new Error('Data must be a Buffer');
+    }
+    this.size = data.length;
+    this.updated = new Date().toISOString();
+    await writeFragmentData(this.ownerId, this.id, data);
+    await this.save();
+  }
+
+  /**
+   * Returns the mime type (e.g., without encoding) for the fragment's type:
+   * "text/html; charset=utf-8" -> "text/html"
+   * @returns {string} fragment's mime type (without encoding)
+   */
+  get mimeType() {
+    const { type } = contentType.parse(this.type);
+    return type;
+  }
+
+  /**
+   * Returns true if this fragment is a text/* mime type
+   * @returns {boolean} true if fragment's type is text/*
+   */
+  get isText() {
+    return this.mimeType.startsWith('text/');
+  }
+
+  /**
+   * Returns the formats into which this fragment type can be converted
+   * @returns {Array<string>} list of supported mime types
+   */
+  get formats() {
+    return [this.mimeType];
+  }
+
+  /**
+   * Returns true if we know how to work with this content type
+   * @param {string} value a Content-Type value (e.g., 'text/plain' or 'text/plain: charset=utf-8')
+   * @returns {boolean} true if we support this Content-Type (i.e., type/subtype)
+   */
+  static isSupportedType(value) {
+    // Expand when adding new types
+    const supportedTypes = ['text/plain'];
+    return supportedTypes.includes(contentType.parse(value).type);
+  }
+}
+
+module.exports.Fragment = Fragment;
